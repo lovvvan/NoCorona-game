bdaa58f0c3c67e455e8e94d0f2f4ef6e62e7f4b2
heytek-96 <34439112+heytek-96@users.noreply.github.com>
2021-05-06 17:00:00 +0200

    Continuing Map Generation
    Slight tweaks to assets

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,7 +6,10 @@ using System;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField]
+    private Transform map;
+    [SerializeField]
     private Texture2D[] mapData;
+    [SerializeField]
     private MapElement[] mapElements;
     [SerializeField]
     private Sprite defaultTile;
@@ -20,13 +23,35 @@ public class LevelManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        GenerateMap();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+    private void GenerateMap()
+    {
+        for (int i = 0; i < mapData.Length; i++)
+        {
+            for (int x = 0; x < mapData[i].width; x++)
+            {
+                for (int y = 0; y < mapData[i].height; y++)
+                {
+                    Color c = mapData[i].GetPixel(x,y);
+                    MapElement newElement = Array.Find(mapElements, e => e.MyColor == c);
+                    if (newElement != null)
+                    {
+                        float xPos = WorldStartPos.x + (defaultTile.bounds.size.x * x);
+                        float yPos = WorldStartPos.y + (defaultTile.bounds.size.y * y);
+                        GameObject go = Instantiate(newElement.MyElementPrefab);
+                        go.transform.position = new Vector2(xPos,yPos);
+                        go.transform.parent = map;
+                    }
+                }
+            }
+        }
     }
 }
 
