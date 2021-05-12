8596779f1579aedcafc29c7d921911a134ac0bfd
lovvvan <lovisa.hedman93@gmail.com>
2021-05-12 15:03:10 +0200

    trying to add an enemy

--- Assets/Scripts/enemy_boy.cs
+++ Assets/Scripts/enemy_boy.cs
@@ -2,22 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy_eggboy : MonoBehaviour
+public class enemy_boy: Character
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        base.Start();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Move();
+        GetInput();
+        base.Update();
     }
 
-    void Move()
+    private void GetInput()
     {
-        transform.Translate(Vector2.down);
+        direction = Vector2.zero;
+        direction += Vector2.down;
+
     }
 }
