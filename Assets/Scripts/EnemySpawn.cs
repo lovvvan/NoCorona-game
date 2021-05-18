bc3275e9b0d6e5e07b511fff2229746addb726d2
heytek-96 <34439112+heytek-96@users.noreply.github.com>
2021-05-18 16:30:32 +0200

    Fixed random enemies

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -31,14 +31,16 @@ public class EnemySpawn : MonoBehaviour
     private void SpawnEnemies()
     {
         // You do -1 because arrays and listsstart at zero but if your array is only 1 long and you have Random.Range(0, spawnZones.Length) it would be (0, 1) because the length is 1 gameobject long.
-        int spawnNum = Random.Range(0, spawnZones.Length - 1); // Grabs a random number
-        int enemyNum = Random.Range(0, enemyPrefabs.Length - 1); // Grabs a random number
+        int spawnNum;
+        int enemyNum;
 
         //Here is where you spawn your enemies, you could spawn heaps of them by using enemyList.Add(enemyPrefabs[enemyNum], spawnZones[spawnNum], Quaternion.identiy); over and over, but you could use
         //A While loop and have a maximum spawn number like so; (I added maxSpawn, change it to whatever number in the inspector)
 
         while(enemyList.Count < maxSpawn)
         {
+            spawnNum = Random.Range(0, spawnZones.Length - 1); // Grabs a random number
+            enemyNum = Random.Range(0, enemyPrefabs.Length - 1); // Grabs a random number
             enemyList.Add(Instantiate(enemyPrefabs[enemyNum], spawnZones[spawnNum].transform));
         }
 
