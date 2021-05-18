23cce2d6f561836140071ab706ea69423c954105
lovvvan <lovisa.hedman93@gmail.com>
2021-05-18 15:51:44 +0200

    added stuff to the camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,15 +6,37 @@ public class CameraController : MonoBehaviour
 {
 
     public GameObject player;        //Public variable to store a reference to the player game object
-
-
     private Vector3 offset;            //Private variable to store the offset distance between the player and camera
+
+    public BoxCollider2D boundBox;
+    private Vector3 minBounds;
+    private Vector3 maxBounds;
+
+    private Camera theCamera;
+    private float halfHeight;
+    private float halfWidth;
+
 
     // Use this for initialization
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
         offset = transform.position - player.transform.position;
+
+        minBounds = boundBox.bounds.min;
+        maxBounds = boundBox.bounds.max;
+
+        theCamera = GetComponent<Camera>();
+        halfHeight = theCamera.orthographicSize;
+        halfWidth = halfHeight * Screen.width / Screen.height;
+
+    }
+
+    void Update()
+    {
+        float clampedX = Mathf.Clamp(transform.position.x, minBounds.x + halfWidth, maxBounds.x - halfWidth);
+        float clampedY = Mathf.Clamp(transform.position.y, minBounds.y + halfHeight, maxBounds.y - halfHeight);
+        transform.position = new Vector3(clampedX, clampedY, transform.position.z);
     }
 
     // LateUpdate is called after Update each frame
