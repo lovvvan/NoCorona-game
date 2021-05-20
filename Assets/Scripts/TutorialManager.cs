c96c600c7006c0fbce4bf23aed8b296950e04fa4
iduberga <idabergman97@gmail.com>
2021-05-20 11:40:58 +0200

    Added tutorial for gameplay

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+  public GameObject[] popUps;
+  private int popUpIndex;
+  public GameObject EnemySpawn;
+  public float waitTime = 4f;
+
+    // Update is called once per frame
+    void Update()
+    {
+      for (int i = 0; i < popUps.Length; i++)
+      {
+        if(i == popUpIndex)
+        {
+          popUps[i].SetActive(true);
+        }
+        else
+        {
+          popUps[i].SetActive(false);
+        }
+      }
+      if(popUpIndex==0)
+      {
+        if(Input.GetKey(KeyCode.W)||Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.A)||Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.S)||Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.D)||Input.GetKey(KeyCode.RightArrow))
+        {
+          popUpIndex++;
+        }
+      }
+      else if(popUpIndex==1)
+      {
+        if(waitTime==2)
+        {
+          EnemySpawn.SetActive(true);
+          waitTime-=Time.deltaTime;
+        }
+        else if(waitTime<=0)
+        {
+          popUpIndex++;
+        }
+        else
+        {
+          waitTime-=Time.deltaTime;
+        }
+      }
+    }
+}
