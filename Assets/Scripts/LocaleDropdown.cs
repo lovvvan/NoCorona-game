1ec55355420c2cebb1517b2632ed6cddc2e89076
lovvvan <lovisa.hedman93@gmail.com>
2021-05-26 14:48:13 +0200

    language should work in build now!

--- Assets/Scripts/LocaleDropdown.cs
+++ Assets/Scripts/LocaleDropdown.cs
@@ -17,14 +17,20 @@ public class LocaleDropdown : MonoBehaviour
         // Generate list of available Locales
         var options = new List<TMP_Dropdown.OptionData>();
         int selected = 0;
+        Debug.Log("nr of localisations:"); 
+        Debug.Log(LocalizationSettings.AvailableLocales.Locales.Count);
         for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i)
         {
             var locale = LocalizationSettings.AvailableLocales.Locales[i];
+            Debug.Log("locale:");
+            Debug.Log(locale);
             if (LocalizationSettings.SelectedLocale == locale)
                 selected = i;
             options.Add(new TMP_Dropdown.OptionData(locale.name));
         }
         dropdown.options = options;
+        Debug.Log("options:");
+        Debug.Log(options);
 
         dropdown.value = selected;
         dropdown.onValueChanged.AddListener(LocaleSelected);
